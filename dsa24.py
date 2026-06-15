c601b88e6dbb681d5030071f1e1b906bbdee96e6
Pratham Kamath <prathamkamath627@gmail.com>
2026-06-15 19:14:58 +0530

    Add files via upload

---
+++ dsa24.py
@@ -0,0 +1,37 @@
+# Simple Doubly Linked List
+
+class Node:
+    def _init_(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+head = None
+
+n = int(input("Enter number of nodes: "))
+
+for i in range(n):
+    x = int(input("Enter data: "))
+
+    new_node = Node(x)
+
+    if head is None:
+        head = new_node
+    else:
+        temp = head
+        while temp.next:
+            temp = temp.next
+
+        temp.next = new_node
+        new_node.prev = temp
+
+# Display Doubly Linked List
+temp = head
+
+print("Doubly Linked List:")
+
+while temp:
+    print(temp.data, end=" <-> ")
+    temp = temp.next
+
+print("None")
