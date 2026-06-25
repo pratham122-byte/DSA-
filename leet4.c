e2bd214921899396a41b12ae3353ca7c42baa723
Pratham Kamath <prathamkamath627@gmail.com>
2026-06-25 22:13:32 +0530

    Add files via upload

---
+++ leet4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
+
+    return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+}
+
+int main() {
+ 
+
+    struct TreeNode* root = createNode(3);
+    root->left = createNode(9);
+    root->right = createNode(20);
+    root->right->left = createNode(15);
+    root->right->right = createNode(7);
+
+    printf("Maximum Depth = %d\n", maxDepth(root));
+
+    return 0;
+}
